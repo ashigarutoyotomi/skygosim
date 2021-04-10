06ec229119c34cb582184503db86a40020964248
Amanzhol <amanzhol.fvfy@gmail.com>
2021-04-11 01:18:16 +0600

    Sim Orders and Dealers

--- resources/js/pages/Admin/Dealer/Sims/PhysicalSims.vue
+++ resources/js/pages/Admin/Dealer/Sims/PhysicalSims.vue
@@ -1,8 +1,7 @@
 <template>
-    <div id="sim-cards">
-        <upload-sim-cards-modal
-            modal-id="UploadSimCardsModal"
-        />
+    <div id="dealer-physical-sims">
+
+        <select-physical-sim-range-modal />
 
         <template v-if="loading">
             <div class="row">
@@ -13,11 +12,11 @@
         </template>
 
         <template v-if="!loading">
-            <div class="internet-packages bg-white shadow-sm rounded-lg py-4 px-4">
+            <div class="internet-packages bg-white shadow-sm rounded-3 py-4 px-4">
                 <div class="row">
                     <div class="col-4">
                         <div class="mb-4">
-                            <h5>Sim Cards ({{data ? data.total : 0}})</h5>
+                            <h5>Physical SIMs ({{data ? data.total : 0}})</h5>
                         </div>
                     </div>
                     <div class="col-8">
@@ -26,11 +25,10 @@
                                 <button
                                     type="button"
                                     class="btn btn-light btn-sm"
-                                    title="Upload new packages"
-                                    data-toggle="modal"
-                                    data-target="#UploadSimCardsModal"
+                                    title="Select sim ICCID range"
+                                    @click="openSelectSimRangeModal"
                                 >
-                                    <i class="bi bi-cloud-arrow-up"></i>
+                                    <i class="bi bi-sim-fill"></i>
                                 </button>
                             </li>
                         </ul>
@@ -41,16 +39,15 @@
                     <div class="col-12">
                         <template v-if="!data.data.length">
                             <div class="text-muted mb-4">
-                                Packages not found
+                                Physical SIMs not found
                             </div>
 
                             <button
                                 type="button"
                                 class="btn btn-light btn-sm"
-                                data-toggle="modal"
-                                data-target="#UploadSimCardsModal"
+                                @click="openSelectSimRangeModal"
                             >
-                                Upload Sim Cards
+                                Select Physical SIMs
                             </button>
                         </template>
 
@@ -59,9 +56,9 @@
                                 <thead>
                                     <tr>
                                         <th scope="col">#</th>
-                                        <th scope="col">ICCID</th>
-                                        <th scope="col">SIM TYPE</th>
                                         <th scope="col">IMSI</th>
+                                        <th scope="col">SIM TYPE</th>
+                                        <th scope="col">ICCID</th>
                                         <th scope="col">PIN2</th>
                                         <th scope="col">PUK1</th>
                                     </tr>
@@ -80,61 +77,69 @@
                         </template>
                     </div>
                 </div>
+
+                <pagination
+                    :data="data"
+                    load-event="dealer-physical-sims.load"
+                />
             </div>
         </template>
     </div>
 </template>
 
 <script>
-import UploadSimCardsModal from "./modals/UploadSimCardsModal";
-import { SIM_TYPES } from "./constants";
+    import { SIM_TYPES } from "../../Inventory/Sims/constants";
+    import Pagination from "../../../../components/admin/pagination";
+    import SelectPhysicalSimRangeModal from "./modals/SelectPhysicalSimRangeModal";
 
-export default {
-    name: "SimCardsIndex",
+    export default {
+        name: "DealerPhysicalSims",
+        components: {SelectPhysicalSimRangeModal, Pagination},
+        props: {
+            dealerId: {
+                type: String,
+                default: () => null,
+            }
+        },
 
-    components: {
-        UploadSimCardsModal,
-    },
+        data () {
+            return {
+                data: {},
+                loading: false,
+                simTypes: SIM_TYPES,
+            }
+        },
 
-    data () {
-        return {
-            data: {},
-            loading: false,
-            simTypes: SIM_TYPES,
-        }
-    },
+        created() {
+            this.$root.$on('dealer-physical-sims.load', this.loadData);
 
-    created() {
-        this.$root.$on('sim-cards.load', this.loadData);
+            this.loadData();
+        },
 
-        this.loadData();
-    },
+        beforeDestroy() {
+            this.$root.$off('dealer-physical-sims.load', this.loadData);
+        },
 
-    beforeDestroy() {
-        this.$root.$off('sim-cards.load', this.loadData);
-    },
+        methods: {
+            loadData() {
+                this.loading = true;
 
-    methods: {
-        loadData() {
-            this.loading = true;
+                axios.get(`/dealers/${this.dealerId}/physical-sims`)
+                    .then(({data}) => {
+                        this.data = data;
+                        this.loading = false;
+                    });
+            },
 
-            axios.get('/sim-cards')
-                .then(({data}) => {
-                    this.data = data;
-                    this.loading = false;
-                });
+            openSelectSimRangeModal() {
+                this.$root.$emit('modal::show::SelectPhysicalSimRangeModal', {
+                    dealer_id: this.dealerId,
+                })
+            }
         }
     }
-}
 </script>
 
 <style lang="scss">
-    .internet-packages {
-        &__actions-list {
-            width: 100%;
-            display: flex;
-            justify-content: flex-end;
-            list-style: none;
-        }
-    }
+
 </style>
