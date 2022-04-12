e033748b80640697e295680857ee99fb6f45cad0
toyotomi <abecodeman@gmail.com>
2022-04-12 16:44:15 +0600

    merged

--- app/Http/Controllers/Auth/AuthController.php
+++ app/Http/Controllers/Auth/AuthController.php
@@ -6,6 +6,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Session;
+use App\Models\User;
 
 
 class AuthController extends Controller
