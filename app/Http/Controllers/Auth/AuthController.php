37558e0e8e6ab57417877cfe565f9365daa50337
toyotomi <abecodeman@gmail.com>
2022-04-12 16:43:07 +0600

    code pasted

--- app/Http/Controllers/Auth/AuthController.php
+++ app/Http/Controllers/Auth/AuthController.php
@@ -10,5 +10,30 @@ use Illuminate\Support\Facades\Session;
 
 class AuthController extends Controller
 {
-    
+    /**
+     * Get auth user data
+     */
+    public function me()
+    {
+        $user = Auth::user();
+
+        if (!$user && !$user->id) {
+            return null;
+        }
+
+        $userQuery = User::query()
+            ->with([
+                'sims.sim'
+            ]);
+
+        if ($user->role === User::USER_ROLE_ADMIN) {
+            return $userQuery->find($user->id);
+        }
+
+        if ($user->role === User::USER_ROLE_DEALER) {
+            return $userQuery->find($user->id);
+        }
+
+        return $userQuery->find($user->id);
+    }
 }
